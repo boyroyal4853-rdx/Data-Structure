cfe46afce7a4affaa1350166125ad82c05953114
diwakar chaudhary <boyroyal4853@gmail.com>
2026-03-16 23:56:36 +0530

    add

---
+++ find_maximum_element.c
@@ -0,0 +1,25 @@
+// #include <stdio.h>
+
+// int main() {
+//     int arr[100], n, i, max;
+
+//     printf("Enter number of elements: ");
+//     scanf("%d", &n);
+
+//     printf("Enter %d elements:\n", n);
+//     for(i = 0; i < n; i++) {
+//         scanf("%d", &arr[i]);
+//     }
+
+//     max = arr[0];   // assume first element is maximum
+
+//     for(i = 1; i < n; i++) {
+//         if(arr[i] > max) {
+//             max = arr[i];
+//         }
+//     }
+
+//     printf("Maximum element in array = %d", max);
+
+//     return 0;
+// }
