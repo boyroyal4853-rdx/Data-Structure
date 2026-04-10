4ee7d7d51c210e708719d92880b418ef9a902b2f
diwakar chaudhary <boyroyal4853@gmail.com>
2026-04-10 22:35:04 +0530

    add

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,28 +1,58 @@
-#include <stdio.h>
+// #include <stdio.h>
+
+// int main()
+// {
+//     int arr[] = {64, 34, 25, 12, 22, 11, 90};
+//     int n = sizeof(arr) / sizeof(arr[0]);
+//     int i, j, temp;
+
+//     for (i = 0; i < n - 1; i++)
+//     {
+//         for (j = 0; j < n - i - 1; j++)
+//         {
+//             if (arr[j] > arr[j + 1])
+//             {
+//                 temp = arr[j];
+//                 arr[j] = arr[j + 1];
+//                 arr[j + 1] = temp;
+//             }
+//         }
+//     }
+
+//     printf("Sorted array (Bubble Sort):\n");
+//     for (i = 0; i < n; i++)
+//     {
+//         printf("%d ", arr[i]);
+//     }
+//     return 0;
+// }
+
+#include<stdio.h>
 
 int main()
 {
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int i, j, temp;
+    int arr[] = {11,22,12,33,55,3,2};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int i,j,temp;
 
-    for (i = 0; i < n - 1; i++)
-    {
-        for (j = 0; j < n - i - 1; j++)
+    for(i=0; i<n-1 ; i++){
+        for(j=0; j<n-i-1; j++)
         {
-            if (arr[j] > arr[j + 1])
+            if (arr[j] > arr[j+1])
             {
                 temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
         }
     }
 
-    printf("Sorted array (Bubble Sort):\n");
-    for (i = 0; i < n; i++)
+    printf("Sorted array (bubble sort):\n");
+
+    for(i=0; i<n; i++)
     {
         printf("%d ", arr[i]);
     }
+
     return 0;
 }
