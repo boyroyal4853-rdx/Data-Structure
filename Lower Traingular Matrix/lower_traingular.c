c64f25bc7b083b604650a4c1abff485b29f4bd47
diwakar chaudhary <boyroyal4853@gmail.com>
2026-05-05 22:34:51 +0530

    add

---
+++ Lower Traingular Matrix/lower_traingular.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int arr[10][10], n, i, j;
+
+    printf("Enter size of square matrix: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    printf("Lower Triangular Matrix:\n");
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            if(j <= i)
+                printf("%d ", arr[i][j]);
+            else
+                printf("  ");  // spacing
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
